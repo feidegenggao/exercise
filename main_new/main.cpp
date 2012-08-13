6ae39db8917b8580aba3349f578cc74ab7b10e0c
Higher <liuxiaofei333@gmail.com>
2012-08-13 17:27:45 +0800

    add main_new

---
+++ main_new/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/13/12 17:21
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Higher (feidegenggao), liuxiaofei333@gmail.com
+ *   Organization:  NDSL UESTC
+ *
+ * =====================================================================================
+ */
+#include   "../my.h"
+
+//can new class here?
+//cout<<"cout comment before main() function"<<endl;
+    int
+main()
+{
+    cout<<"Hello world!"<<endl;
+    return 0;
+}
