fcb4be221a915932168a6c18ad5ffb60f0c2f11d
Higher <liuxiaofei333@gmail.com>
2012-08-16 23:08:59 +0800

    add file

---
+++ cin_cout/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  test cin
+ *
+ *        Version:  1.0
+ *        Created:  08/16/12 22:55
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Higher (feidegenggao), liuxiaofei333 at gmail dot com
+ *   Organization:  NDSL UESTC
+ *
+ * ============================================================================
+ */
+#include    "../my.h"
+
+int main(void)
+{
+    int a = 1;
+    cout<<"a:"<<a<<endl;
+    cin>>a;
+    cout<<"a:"<<a<<endl;
+
+    return 0;
+}
