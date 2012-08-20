face9e82b6f9a2e365a2ec0eab5a28c7498ee4e8
root <root@Live-89.(none)>
2012-08-20 20:04:07 +0800

    add files in 8-20

---
+++ autoconf/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/20/12 17:05:50
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lxf (), 
+ *        Company:  NDSL
+ *
+ * ============================================================================
+ */
+#include    "sayhello.h"
+
+int main()
+{
+    sayhello();
+
+    return 0;
+}
---
+++ autoconf/sayhello.h
@@ -0,0 +1,25 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  sayhello.h
+ *
+ *    Description:  header of sayhello
+ *
+ *        Version:  1.0
+ *        Created:  08/20/12 17:04:16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lxf (), 
+ *        Company:  NDSL
+ *
+ * ============================================================================
+ */
+#ifndef     _SAY_HELLO_HEADER
+#define     _SAY_HELLO_HEADER
+#include    <iostream>
+using namespace std;
+using std::cout;
+using std::endl;
+int sayhello();
+#endif
