5f45de899a7b7258fff95f89e8a76e283056b08c
Hihger <liuxiaofei333@gmail.com>
2012-11-01 09:50:21 +0800

    修改了，隐式类型转换

--- explicit/main.cpp
+++ explicit/main.cpp
@@ -21,20 +21,28 @@ class lock
 {
     public:
         explicit lock(int nu):ele(nu) {cout<<"new lock"<<endl;}
+        lock(const lock& ) { cout<<"copy constructor"<<endl;}
         ~lock(){cout<<"destruct lock"<<endl;}
 
-        void print(void) { cout<<"ele:"<<ele<<endl;}
+        void print(void) const { cout<<"ele:"<<ele<<endl;}
 
     private:
         int ele;
 };
 
+void fun(const lock& a)
+{
+    cout<<"fun"<<endl;
+    a.print();
+    cout<<"fun end"<<endl;
+}
+
 int main()
 {
-    lock* plock = new lock(2);
-
-    plock->print();
-
-    delete plock;
-
+    lock plock(2);
+    fun(plock);
+    //如果lock的构造函数，没有explicit限制符的话，下面的函数是可以通过编译的。
+    //这就是c++中的隐式转换
+    //如果有explicit限制符的话，编译时通不过的。
+    fun(2);
 }
