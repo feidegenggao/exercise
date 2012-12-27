37fb3412159a47c60724ba4bf3e1dc79a76968d0
Higher <liuxiaofei333@gmail.com>
2012-12-27 21:42:33 +0800

    add some files

--- macro/main.cpp
+++ macro/main.cpp
@@ -1,10 +1,29 @@
 #include    "../my.h"
+#include    <stdio.h>
 
-//#define     DEBUG
+#define     DEBUG
+#define     TEST(EXP) #EXP
+//TEST(hello) -->> "hello"
+//add " before and behind EXP
+#define     TOKEN(A, B) L##A##B
+//TOKEN (a, b) -->> Lab
+//connect L and a and b
+#define     myprintf(fmt,...) fprintf(stderr, fmt, ##__VA_ARGS__)
+#define     mynoprintf(fmt,...) fprintf(stderr, fmt, __VA_ARGS__)
+//This myprintf can't output :myprintf("Hello"); correctly
+//You should add ## before __VA_ARGS__
+#define     argprintf(fmt,arg...) fprintf(stderr, fmt, ##arg)
 int main()
 {
     #ifdef  DEBUG
     cout<<"define DEBUG"<<endl;
     #endif
 
+    printf("words:%s\n", TEST(hello));
+    //cout<<TOKEN(a,b)<<endl;
+    myprintf("log:%d\n", 1);
+    myprintf("log:hi\n");
+    mynoprintf("log:hi\n");
+    argprintf("log:%d\n", 1);
+    argprintf("log:hi\n");
 }
